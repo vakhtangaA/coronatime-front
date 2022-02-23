677b35ffd32618d0a0d70a4246d709f3df47ad68
vakhtangaa <vakhtang.chitauri@gmail.com>
2022-02-23 10:27:43 +0400

    wip(testing): withCredentials -> credentials in axios

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const apiClient = axios.create({
   baseURL: process.env.REACT_APP_PUBLIC_API,
-  withCredentials: true,
+  crednetials: true,
 });
 
 export default apiClient;
